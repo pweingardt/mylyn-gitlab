a6e0b967d6a162c88945a1693d5048af2f29fc8d
Paul Weingardt <weingardt.dev@gmail.com>
2014-03-12 12:52:58 +0100

    added license headers

--- de.weingardt.mylyn.gitlab.ui/src/de/weingardt/mylyn/gitlab/ui/GitlabConnectorUI.java
+++ de.weingardt.mylyn.gitlab.ui/src/de/weingardt/mylyn/gitlab/ui/GitlabConnectorUI.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2014, Paul Weingardt
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Paul Weingardt - initial API and implementation
+ *******************************************************************************/
+
+
+
 package de.weingardt.mylyn.gitlab.ui;
 
 import org.eclipse.jface.resource.ImageDescriptor;
